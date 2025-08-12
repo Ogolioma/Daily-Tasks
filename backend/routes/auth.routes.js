5f2eaa8f9045570b10638522237d8a33f88c078b
Gladys <dualukeg@gmail.com>
2025-08-12 18:06:25 +0100

    update reset password

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,10 +4,14 @@ const {
   registerUser,
   createPassword,
   loginUser,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/authController");
 
 router.post("/register", registerUser);
 router.post("/confirm-email/:token", createPassword);
 router.post("/sign-in", loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
