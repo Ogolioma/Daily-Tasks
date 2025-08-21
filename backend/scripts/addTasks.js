4d22d342403a0965315bfeeae850e4faeb9a9546
Gladys <dualukeg@gmail.com>
2025-08-21 21:27:24 +0100

    update policy

--- backend/scripts/addTasks.js
+++ backend/scripts/addTasks.js
@@ -15,13 +15,13 @@ mongoose.connect(process.env.MONGO_URI)
         actionLink: "https://prnt.sc/VBlBjFPzesgh",
         points: 6,
         questions: [
-          {question: "Basically being a Human _________ for the internet", answer: "compass"},
-          {question: "The internet is a _________ if you know where to dig", answer: "goldmine"},
-          { question: "Financial freedom is about _______", answer: "discipline" },
-          {question: "He felt like his __________ had a secret escape plan", answer: "money"},
-          {question: "It was search engine _________", answer: "evaluation"},
-          {question: "The _________ wasn't about rushing through task", answer: "secret"},
-          {question: "After a ___________ John was debt-free", answer: "year"}
+          {question: "John earned enough to pay off his _______ bill and groceries", answer: "phone"},
+          {question: "__________ doesn't have to be your forever story", answer: "debt"},
+          { question: "_______ doesn't have to be a dream", answer: "stability" },
+          {question: "The MilkyWay project can be the _________ to your new beginning", answer: "launchpad"},
+          {question: "His _________ never seemed to shrink", answer: "loans"},
+          {question: "Like anyone _________ for answers", answer: "desperate"},
+          {question: "He got ________ by the rules", answer: "confused"}
         ]
       },
       {
@@ -30,13 +30,13 @@ mongoose.connect(process.env.MONGO_URI)
         actionLink: "https://prnt.sc/7kPmdPMGudHi",
         points: 6,
         questions: [
-          {question: "Also, remember to ______ your hours", answer: "track"},
-          {question: "It was like being the _________ of the internet", answer: "referee"},
-          { question: "No _______ delays, No excuses", answer: "shady" },
+          {question: "My ________ can help you pass the test for just a little fee", answer: "specialist"},
+          {question: "_________ change and so should you", answer: "guidelines"},
+          { question: "You'll then see _______ buttons", answer: "four" },
           {question: "All it takes is a ________ application", answer: "brief"},
-          {question: "Step three, Click _________ more", answer: "view"},
-          {question: "It's your _________ to working from anywhere at anytime", answer: "ticket"},
-          {question: "________ your residential proxy and start today", answer: "grab"}
+          {question: "Read the job _________ and click 'Apply Now'", answer: "description"},
+          {question: "It'll ask you to complete ________ certifications", answer: "two"},
+          {question: "It's your ________ to working anywhere at anytime", answer: "ticket"}
         ]
       },
        {
@@ -45,12 +45,12 @@ mongoose.connect(process.env.MONGO_URI)
         actionLink: "https://prnt.sc/1Bnv2RPRpwRu",
         points: 6,
         questions: [
-          {question: "Jude used his new _________ wisely", answer: "income"},
-          {question: "Your _________ has to stay consistent", answer: "profile"},
-          { question: "Top Surveys will start sending you surveys ______", answer: "daily" },
-          {question: "Be _______ but not rushed", answer: "fast"},
-          {question: "Minimun payout is ________ dollar on the first withdraw", answer: "five"},
-          {question: "If You're ______ you might as well get paid for it", answer: "scrolling"}
+          {question: "Jude didn't just make money, He made ________", answer: "progress"},
+          {question: "Use a ___________ country", answer: "supported"},
+          { question: "Take ______ minutes and answer every question honestly (write in words)", answer: "ten" },
+          {question: "_________ quickly but logically", answer: "answer"},
+          {question: "Avoid contradictions, they use ________ questions", answer: "trick"},
+          {question: "Your ______ is processed instantly", answer: "payout"}
         ]
       },
       {
@@ -59,12 +59,12 @@ mongoose.connect(process.env.MONGO_URI)
         actionLink: "https://prnt.sc/2siykl5fsApo",
         points: 6,
         questions: [
-          {question: "Tobi didn't finish his project because he was a tech _________", answer: "genius"},
-          {question: "Letting experts _________ their lane", answer: "handle"},
-          { question: "______ don't reward effort", answer: "people" },
-          {question: "You'll be _________ what money well spent can do", answer: "shocked"},
-          {question: "Daily Tasks has the community to make it ____________", answer: "happen"},
-          {question: "Daily Tasks, that's _________ with power", answer: "strategy"}
+          {question: "A way to _________ your hustle to a team that lives for results", answer: "outsource"},
+          {question: "Daily Tasks has built a solid _________", answer: "process"},
+          { question: "Use the team that was built to help you _______", answer: "win" },
+          {question: "Let me take a little ________ here", answer: "detour"},
+          {question: "And those are _____ questions", answer: "fair"},
+          {question: "You don't _______ and disappear", answer: "pay"}
         ]
       },
        {
@@ -73,12 +73,12 @@ mongoose.connect(process.env.MONGO_URI)
         actionLink: "https://prnt.sc/gFq7kjTUzZad",
         points: 6,
         questions: [
-          {question: "Don't _________ your engagers", answer: "ghost"},
-          {question: "Don't just build clout, turn it into ________", answer: "coin"},
-          { question: "Thanks for ______ around this long", answer: "sticking" },
-          {question: "Bring something to the ____________", answer: "table"},
-          {question: "He didn't pray to the ____________ gods", answer: "algorithm"},
-          {question: "Daily Tasks isn't a ___________ machine", answer: "miracle"}
+          {question: "More views equals better _________ ad revenue", answer: "youtube"},
+          {question: "Don't ________ links", answer: "spam"},
+          { question: "Don't _________ after getting your engagement", answer: "ghost" },
+          {question: "He built friendships and got ________ that made him better", answer: "feedback"},
+          {question: "Your not going to become Mr __________ overnight", answer: "beast"},
+          {question: "If you've ever felt like your ___________ was invisible", answer: "content"}
         ]
       },
        {
@@ -87,88 +87,63 @@ mongoose.connect(process.env.MONGO_URI)
         actionLink: "https://prnt.sc/sMxY77xrS3NW",
         points: 6,
         questions: [
-          {question: "Don't _________ your engagers", answer: "ghost"},
-          {question: "Don't just build clout, turn it into ________", answer: "coin"},
-          { question: "Thanks for ______ around this long", answer: "sticking" },
-          {question: "Bring something to the ____________", answer: "table"},
-          {question: "He didn't pray to the ____________ gods", answer: "algorithm"},
-          {question: "Daily Tasks isn't a ___________ machine", answer: "miracle"}
+          {question: "Answer __________ questions for the short videos", answer: "attention"},
+          {question: "There is a ______ way to do this", answer: "smart"},
+          { question: "The _________ has trust issues ", answer: "internet" },
+          {question: "You're ________ online, why not get paid", answer: "already"},
+          {question: "_________ may not grow on trees, but it may go on screens", answer: "money"}
+        ]
+      },
+      {
+        title: "Watch Video",
+        instructions: "Go to 'youtube.com', search for '7 ways to make money'. Type it in the searchbox, do not copy and paste.<br/><br/> Find the video with the thumbnail that appears when you click 'Go to task'.<br/<> Watch the video till the end and answer the questions. <br/><br/>Please, give one-word answers and in all small cases/letters",
+        actionLink: "https://prnt.sc/r_KVf5b-te_H",
+        points: 6,
+        questions: [
+          {question: "It's simple you get paid for _________", answer: "engagement"},
+          {question: "It's Bout learning how to create _______", answer: "value"},
+          { question: "_________ opportunities are everywhere", answer: "offline" },
+          {question: "People pay for __________", answer: "convenience"},
+          {question: "Your _________ is in your combo, not perfection", answer: "edge"}
         ]
       },
        {
         title: "Watch Video",
-        instructions: "Watch the video to the end without skipping,like and leave a relevant comment.<br/><br/> Upload screenshot showing you watched the video to end, with your comment. <br/><br/>Answer the questions. Please, write your answer in small-cases (small letters) and give one-word answers",
-        actionLink: "https://youtu.be/t4NWv3tShcA?si=P1OM0p-pC57aeW4Y",
+        instructions: "Go to 'youtube.com', search for 'how to scale your income fast'. Type it in the searchbox, do not copy and paste.<br/><br/> Find the video with the thumbnail that appears when you click 'Go to task'.<br/<> Watch the video till the end and answer the questions. <br/><br/>Please, give one-word answers and in all small cases/letters",
+        actionLink: "https://prnt.sc/wqkdwhUcG_Qn",
         points: 6,
         questions: [
-          { question: "Start _________, improve as you go?", answer: "messy" },
-          {question: "It puts ________ cash in your pocket and helps build momentum?", answer: "consistent"},
-          {question: "_________ plus good voice equals voice overs?", answer: "phone"},
-          {question: "No degree, no big capital, just ________", answer: "hustle"},
-          {question: "________ what you know", answer: "teach"},
-          { question: "We're all out here trying to ________, hustle, and win together?", answer: "grow" }
+          {question: "There is __________ in focus", answer: "power"},
+          {question: "Your attention span is _________", answer: "currency"},
+          { question: "_________ instead of doing", answer: "overthinking" },
+          {question: "Ler's say you start with ________ design", answer: "freelance"},
+          {question: "You sell those templates as a _________", answer: "product"}
         ]
       },
       {
         title: "Watch Video",
-        instructions: "Watch the video to the end without skipping,like and leave a relevant comment.<br/><br/> Upload screenshot showing you watched the video to end, with your comment. <br/><br/>Answer the questions. Please, write your answer in small-cases (small letters) and give one-word answers",
-        actionLink: "https://youtu.be/KLG6Jtw_uUo?si=794WcE9MnbgnwVwW",
+        instructions: "Go to 'youtube.com', search for 'Money mastery video'. Type it in the searchbox, do not copy and paste.<br/><br/> Find the video with the thumbnail that appears when you click 'Go to task'.<br/<> Watch the video till the end and answer the questions. <br/><br/>Please, give one-word answers and in all small cases/letters",
+        actionLink: "https://prnt.sc/Zc2T2xzyqdZY",
         points: 6,
         questions: [
-          { question: "Avoid the shiny object ________?", answer: "syndrome" },
-          {question: "Nail it, _________ it, stack it?", answer: "systemize"},
-          {question: "Let's say you start with _________ design", answer: "freelance"},
-          {question: "____________ doesn't come from more chaos, it comes from clean stacking", answer: "scale"},
-          {question: "It's not the ______ that is broken, it's the consistency", answer: "idea"},
-          {question: "Do one thing today that gets you closer to your ________?", answer: "goal"}
+          {question: "Pay _________ first", answer: "yourself"},
+          {question: "Less temptation equlas more ________", answer: "consistency"},
+          { question: "Money flows where ______ lives", answer: "peace" },
+          {question: "five. Ignoring financial _________", answer: "literacy"},
+          {question: "the ________ to take small steps consistently", answer: "courage"}
         ]
       },
       {
         title: "Watch Video",
-        instructions: "Watch the video to the end without skipping,like and leave a relevant comment.<br/><br/> Upload screenshot showing you watched the video to end, with your comment. <br/><br/>Answer the questions. Please, write your answer in small-cases (small letters) and give one-word answers",
-        actionLink: "https://youtu.be/aOUoHqA4A0o?si=0i0loYJ4ze6ji1kG",
-        points: 6,
+        instructions: "Go to 'youtube.com', search for 'Milkyway Part two simulator'. Type it in the searchbox, do not copy and paste.<br/><br/> Find the video with the thumbnail that appears when you click 'Go to task'.<br/<> Watch the video till the end and answer the questions. <br/><br/>Please, give one-word answers and in all small cases/letters",
+        actionLink: "https://prnt.sc/Of8fGHm9z2t3",
+        points: 16,
         questions: [
-          { question: "That's the blueprint for __________ freedom", answer: "financial" },
-          {question: "That could be ___________ your expenses ?", answer: "tracking"},
-          {question: "It doesn't have to be _________ you just have to begin", answer: "perfect"},
-          {question: "__________ in silence, let your success speak for you", answer: "flex"},
-          {question: "You just need clarity, discipline, and the courage to take small steps ________", answer: "consistently"},
-          {question: "Go out there and ________ it, grow it, and most importantly keep it", answer: "earn"}
-        ]
-      },
-       {
-        title: "Watch Video",
-        instructions: "Watch the video to the end without skipping,like and leave a relevant comment.<br/><br/> Upload screenshot showing you watched the video to end, with your comment. <br/><br/>Answer the questions. Please, write your answer in small-cases (small letters) and give one-word answers",
-        actionLink: "https://youtu.be/EFjamX2-SkM?si=PHY_eyRHFTA4hNEU",
-        points: 6,
-        questions: [
-          { question: "___________ is your best friend here", answer: "coffee" },
-          {question: "Every ________ was an opportunity to get better", answer: "challenge"},
-          {question: "Find the MilkyWAy ____________", answer: "project"},
-          {question: "He's a __________ in writing MilkyWay certifications", answer: "specialist"},
-          {question: "Grab your residential ____________, and start today", answer: "proxy"},
-          {question: "You'll receive an ________ from the Oneforma recruitment team", answer: "invite"}
-        ]
-      },
-      {
-        title: "Facebook Share",
-        instructions: "Share the video to Facebook Only",
-        actionLink: "https://youtu.be/eGzf2kFTB10?si=obKdKirVYztGX_gR",
-        points:3
-      },
-      {
-        title: "Watch Video",
-        instructions: "Watch the video to the end without skipping,like and leave a relevant comment.<br/><br/> Upload screenshot showing you watched the video to end, with your comment. <br/><br/>Answer the questions. Please, write your answer in small-cases (small letters) and give one-word answers",
-        actionLink: "https://youtu.be/eGzf2kFTB10?si=obKdKirVYztGX_gR",
-        points: 10,
-        questions: [
-          { question: "I'm tired of ________ noodles too", answer: "instant" },
-          {question: "You might be ________ about bills", answer: "stressed"},
-          {question: "____________ doesn't have to be your forever story", answer: "debt"},
-          {question: "__________ doesn't have to be a dream", answer: "stability"},
-          {question: "____________ realised something", answer: "john"},
-          {question: "No ________ stress eating him alive", answer: "financial"}
+          {question: "You might ask why _____ accuracy matter ", answer: "pin"},
+          {question: "For ________ queries do not force navigational intent", answer: "ambiguous"},
+          { question: "________ if the result is the right place", answer: "evaluate" },
+          {question: "When to make incorrect. If the name is _________", answer: "fabricated"},
+          {question: "Let's look at feautures without a _________", answer: "rooftop"}
         ]
       }
       ]);
